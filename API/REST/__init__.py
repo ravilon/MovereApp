c12f67d36eb3ce4d182307b7401c1370417f42b2
Rávilon <ravilon.ads@inf.ufpel.edu.br>
2025-03-13 08:29:43 -0300

    Creating and testing check db function

--- API/REST/__init__.py
+++ API/REST/__init__.py
@@ -15,14 +15,4 @@ def create_app():
     # Initialize Swagger
     Swagger(app)
     
-    """
-    # Test DB
-    # db = get_db()
-    # collection_ref = db.collection("users")
-    # docs = collection_ref.stream()
-    for doc in docs:
-        print(f"Document ID: {doc.id}")
-        print(f"Data: {doc.to_dict()}")
-    """ 
-    
     return app
--- API/REST/functions/db_check.py
+++ API/REST/functions/db_check.py
@@ -1,12 +1,16 @@
 from REST import database
 
-def calculate():
+def calculate(params):
     # Test DB
     db = database.get_db()
     collection_ref = db.collection("users")
     docs = collection_ref.stream()
+    output = "Results: \n"
+        
     for doc in docs:
         print(f"Document ID: {doc.id}")
         print(f"Data: {doc.to_dict()}")
+        output += f"Document ID: {doc.id} \n"
+        output += f"Data: {doc.to_dict()} \n"
     
-    return collection_ref
+    return output
